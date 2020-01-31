98b2f1b1a881a9deea3a296992be1f879d573379
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-31 13:51:30 -0600

    Adds the displayCountries function

--- script.js
+++ script.js
@@ -5,10 +5,39 @@
 // Filter
 // Modal
 
+const countriesEl = document.getElementById('countries');
+
+getCountries();
+
 async function getCountries() {
   const res = await fetch('https://restcountries.eu/rest/v2/all');
   const countries = await res.json();
 
-}
+  displayCountries(countries);
+};
 
-getCountries();
+
+function displayCountries(countries) {
+  console.log(countries);
+
+  countries.forEach(country => {
+
+    const countryEl = document.createElement('div');
+    countryEl.classList.add('card');
+
+    countryEl.innerHTML = `
+    <div>
+      <img src="https://restcountries.eu/data/deu.svg" alt="">
+    </div
+    <div class="card-body">
+      <h2>Germany</h2>
+      <p><strong>Population:</strong> 81,700,000</p>
+      <p><strong>Region:</strong> Europe</p>
+      <p><strong>Capital:</strong> Berlin</p>
+    </div>
+    `;
+
+    countriesEl.appendChild(countryEl);
+  });
+};
+
